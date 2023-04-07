86346b84b5d6997b40e38acae2ba0ad9f02065df
“chun-deng <d267053@me.com>
2023-04-07 19:12:59 +0200

    add toolbox and biomass data

---
+++ toolbox/tool.py
@@ -0,0 +1,28 @@
+import json
+from pkg_resources import resource_filename, resource_string
+
+# Get the path to the data file
+dict_no_en = resource_filename(__name__, 'data/dict_no_en.csv')
+
+####################################################################
+def translation_dicts(book='no_en'):
+    import pandas as pd
+    if book=='no_en':
+        df = dict_no_en
+        
+    dict_df=pd.read_csv(df)    
+    result=dict(zip(dict_df['from'],dict_df['to']))
+    
+    return result
+####################################################################
+# google translate
+def translate(string='hello', src='no', dest='en'):
+    from googletrans import Translator
+    translator = Translator()
+    result = translator.translate(string, src='no', dest='en')
+    try:
+        return result.text
+    except:
+        return string
+    
+
