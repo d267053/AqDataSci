ffb211451808a0f7185fec8414b8e7f3d1ca1557
“chun-deng <d267053@me.com>
2023-04-07 21:27:26 +0200

    update dictionary and related scripts

--- toolbox/tool.py
+++ toolbox/tool.py
@@ -3,12 +3,15 @@ from pkg_resources import resource_filename, resource_string
 
 # Get the path to the data file
 dict_no_en = resource_filename(__name__, 'data/dict_no_en.csv')
+dict_county_region = resource_filename(__name__, 'data/dict_county_region.csv')
 
 ####################################################################
 def translation_dicts(book='no_en'):
     import pandas as pd
     if book=='no_en':
         df = dict_no_en
+    if book=='dict_county_region':
+        df = dict_county_region
         
     dict_df=pd.read_csv(df)    
     result=dict(zip(dict_df['from'],dict_df['to']))
